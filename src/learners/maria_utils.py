75d64cb0bb95e7b8abb7ae90dbadaac473a4511d
masa5983 <maria.sapounidou@umu.se>
2025-05-06 15:30:21 +0200

    modify files for exercise 2

--- src/learners/maria_utils.py
+++ src/learners/maria_utils.py
@@ -1,8 +1,13 @@
 def is_zero(number):
-    """Testing if the number is zero."""
-    if not isinstance(number, int):
-        raise TypeError("'number' must be of type int.")
-    if number == 0:
-        return True
-    return False
+     """Testing if the number is zero."""
+     if not isinstance(number, int):
+         raise TypeError("'number' must be of type int.")
+     if number == 0:
+         return True
+     return False
 
+def is_prime(num):
+    for n in range(2,int(num**0.5)+1):
+        if num%n==0:
+            return False
+    return True
--- tests/test_maria_utils.py
+++ tests/test_maria_utils.py
@@ -1,24 +1,28 @@
 """Tests all code in src.learners.maria_utils."""
 import unittest
 
-from src.learners.maria_utils import is_zero
+from src.learners.maria_utils import is_zero, is_prime
 
 class TestMariaUtils(unittest.TestCase):
 
-    """Class to test the code in src.bacsim.sven_utils."""
+    """Class to test the code in src.learners.maria_utils."""
 
     def test_is_zero_has_documentation(self):
-        """The function 'is_zero' has documentation."""
-        self.assertTrue(is_zero.__doc__)
-        self.assertIsNotNone(is_zero.__doc__)
+         """The function 'is_zero' has documentation."""
+         self.assertTrue(is_zero.__doc__)
+         self.assertIsNotNone(is_zero.__doc__)
 
     def test_is_zero_responds_correctly_to_ints(self):
-        """The function 'is_zero' responds correctly to integers."""
-        self.assertTrue(is_zero(0))
-        self.assertFalse(is_zero(1))
+         """The function 'is_zero' responds correctly to integers."""
+         self.assertTrue(is_zero(0))
+         self.assertFalse(is_zero(1))
 
     def test_is_zero_raises_an_exception_upon_non_ints(self):
-        """The function 'is_zero' raises an exception upon non-ints."""
-        self.assertRaises(TypeError, is_zero, {1, 2})
-        self.assertRaises(TypeError, is_zero, "I am a string")
+         """The function 'is_zero' raises an exception upon non-ints."""
+         self.assertRaises(TypeError, is_zero, {1, 2})
+         self.assertRaises(TypeError, is_zero, "I am a string")
 
+    def test_is_prime(self):
+        """The function 'is_zero' responds correctly to integers."""
+        self.assertTrue(is_prime(2))
+        self.assertFalse(is_prime(6))
