89e9ded89137f228ad788f5c3014f45c493dd002
warcard <oscarlb@kth.se>
2025-05-06 12:00:07 +0200

    Passes non-integer case. Not passing wrong type case.

--- learners/oscar_and_tanya/is_even.py
+++ learners/oscar_and_tanya/is_even.py
@@ -1,6 +1,8 @@
 
 def is_even(value):
     """Checks if the value is even"""
+    if abs(int(value) - value) > 0:
+        raise ValueError("Non-integer value given")
     return value % 2 == 0
 
 # Tests for is_even
@@ -9,4 +11,9 @@ assert is_even.__doc__
 assert is_even(4)
 assert not is_even(3)
 
-assert is_even(3.5), "not well defined for non-integer numbers"
+try:
+    is_even(3.5) # "not well defined for non-integer numbers"
+except ValueError:
+    assert True
+else:
+    assert False
