b84b843ffaba8989bfe1605a38ec31408626b045
Ida Karlsson <write.ida@gmail.com>
2025-05-07 14:52:11 +0200

    Update is_number.py
    Removed old comment

--- src/learners/is_number.py
+++ src/learners/is_number.py
@@ -2,9 +2,4 @@ def is_number(x):
     """
     This function tests whether the input variable x is a number or not.
     """
-    # if isinstance(x, (int, float)):
-    #     return True
-    # else:
-    #     return False
-
     return isinstance(x, (int, float))
