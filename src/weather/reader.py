a39dc4c4de0560ccd2043f9a26e8515adba3a534
milena-t <milena.trabert@t-online.de>
2025-05-07 15:45:15 +0200

    add comments to reader.py

--- src/weather/reader.py
+++ src/weather/reader.py
@@ -2,10 +2,13 @@
 
 def read_data(filepath:str):
     """Read the weather data from file."""
+
+    # check if file exists at specified path
     import os 
     if not os.path.isfile(filepath):
         return False
 
+    # see if file can be read into a list (each element is one line)
     with open(filepath, "r") as infile:
         infile_list = infile.readlines()
     
