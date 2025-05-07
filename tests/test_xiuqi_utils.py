f304cc57b9fc45e13606dfc22acbad62675fed2b
xiuqij <lostld97@gmail.com>
2025-05-07 17:53:30 +0200

    Issue#53 add function and tests is_datetime

--- tests/test_xiuqi_utils.py
+++ tests/test_xiuqi_utils.py
@@ -3,6 +3,7 @@ import unittest
 
 from src.learners.xiuqi_utils import is_zero
 from src.learners.xiuqi_utils import isprime
+from src.learners.xiuqi_utils import is_datetime
 
 class TestXiuqiUtils(unittest.TestCase):
 
@@ -27,3 +28,18 @@ class TestXiuqiUtils(unittest.TestCase):
         """The function 'is_prime' identifies prime and non prime numbers. """
         self.assertTrue(isprime(7))
         self.assertFalse(isprime(8))
+    
+    def test_is_datetime_detects_datetime(self):
+        """The function 'is_datetime' detectes a datetime correctly."""
+        self.assertTrue(is_datetime('2025-02-28 16:40'))
+    
+    def test_is_datetime_raises_an_exception_upon_non_strings(self):
+        """The function 'is_datetime' works on a string. """
+        self.assertRaises(TypeError,is_datetime,20250228)
+        self.assertRaises(TypeError,is_datetime,['2025-02-28 16:40','2025-03-28 16:40'])
+    
+    def test_is_datetime_works_on_correct_string_length(self):
+        """The function 'is_datetime' works on a string of the correct length (16). """
+        self.assertFalse(is_datetime('nonsense'))
+        self.assertFalse(is_datetime('2025-2-28 16:40'))
+        self.assertTrue(is_datetime('2025-04-28 16:40'))
