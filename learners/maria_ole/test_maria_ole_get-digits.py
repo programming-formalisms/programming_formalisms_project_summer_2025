5a703e9e05d4793f76d805a4cdff7eb3fb0f209f
ole <ole@t470s>
2025-05-07 14:40:12 +0200

    First stab at a test

---
+++ learners/maria_ole/test_maria_ole_get-digits.py
@@ -0,0 +1,14 @@
+"""Test 'get_digits.py in learners.maria_ole"""
+import unittest
+
+from learners.maria_ole import get_digits
+
+class TestRichel(unittest.TestCase):
+
+    """Class to test the code in learners.maria_ole"""
+
+    def test_get_digits(self):
+        """The function 'get_digits' does what it is supposed to."""
+        self.assertIsInstance(get_digits(123), list)
+        self.assertTrue(get_digits(123)[0] == 1)
+        self.assertTrue(len(get_digits(123)) == 3)
