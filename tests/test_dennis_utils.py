d070d914c2e37ef9addf9284578218c306043e0c
dennisleenheer <dennis.leenheer@gmail.com>
2025-05-07 09:28:34 +0200

    Tests Day 2

---
+++ tests/test_dennis_utils.py
@@ -0,0 +1,23 @@
+"""Tests all code in src.bacsim.dennis_utils."""
+import unittest
+
+from src.bacsim.dennis_utils import is_zero
+
+class TestdennisUtils(unittest.TestCase):
+
+    """Class to test the code in src.bacsim.sven_utils."""
+
+    def test_is_zero_has_documentation(self):
+        """The function 'is_zero' has documentation."""
+        self.assertTrue(is_zero.__doc__)
+        self.assertIsNotNone(is_zero.__doc__)
+
+    def test_is_zero_responds_correctly_to_ints(self):
+        """The function 'is_zero' responds correctly to integers."""
+        self.assertTrue(is_zero(0))
+        self.assertFalse(is_zero(1))
+
+    def test_is_zero_raises_an_exception_upon_non_ints(self):
+        """The function 'is_zero' raises an exception upon non-ints."""
+        self.assertRaises(TypeError, is_zero, {1, 2})
+        self.assertRaises(TypeError, is_zero, "I am a string")
