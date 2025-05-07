5262ba9f1fa6ec23ae3cf3a753db96af133ae00d
artto16 <artur.tozzi@liu.se>
2025-05-07 15:41:06 +0200

    Started function roman_to_numeral

--- src/learners/utils_ao.py
+++ src/learners/utils_ao.py
@@ -9,3 +9,11 @@ def is_roman_numeral(numeral):
         if s not in valid_nums:
             return False
     return True
+
+
+def roman_to_numeral(roman):
+    """Converts a single roman numeral into an integer"""
+    roman_num = ["I","V","X","L","C","D","M"]
+    int_num = [1, 5, 10, 50, 100, 500, 1000]
+
+    #TODO: add buffer for numbers to subtract and numbers to add before calculating the integer
