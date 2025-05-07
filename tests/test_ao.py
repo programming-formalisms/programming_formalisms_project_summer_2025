32db3809efb9a01ae9a5dd158ccc4770338fc28f
warcard <oscarlb@kth.se>
2025-05-07 15:32:51 +0200

    add test of roman_to_int

--- tests/test_ao.py
+++ tests/test_ao.py
@@ -17,4 +17,22 @@ class TestUtilsAO(unittest.TestCase):
         self.assertTrue(is_roman_numeral('X'))
         self.assertTrue(is_roman_numeral('L'))
         self.assertFalse(is_roman_numeral('A'))
-        
+
+from src.learners.utils_ao import roman_to_numeral
+
+class TestRomanUtilsAO(unittest.TestCase):
+
+    "Class to test roman conversion in src.learners.utils_ao"
+
+    def test_roman_to_int(self):
+        """ Checks a few conversion cases from roman numerals to int"""
+        self.assertEqual(roman_to_numeral("I"), 1)
+        self.assertEqual(roman_to_numeral("V"), 5)
+        self.assertEqual(roman_to_numeral("X"), 10)
+        self.assertEqual(roman_to_numeral("L"), 50)
+        self.assertEqual(roman_to_numeral("IX"), 9)
+        self.assertEqual(roman_to_numeral("XX"), 20)
+        self.assertEqual(roman_to_numeral("LX"), 60)
+        self.assertEqual(roman_to_numeral("IXL"), 59)
+        self.assertEqual(roman_to_numeral("LXXX"), 80)
+        self.assertEqual(roman_to_numeral("XLIV"), 44)
