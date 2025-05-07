337c79a80be0ed1dddbea0d5c9514352d90813cc
artheisten <art3mij@gmail.com>
2025-05-07 15:13:53 +0200

    input type check: progress #40

--- src/weather/utils.py
+++ src/weather/utils.py
@@ -1,3 +1,7 @@
 def celsius2kelvin(x):
     """Function to convert Celsius to Kelvin"""
+
+    if not isinstance(x, (int, float)):
+        raise TypeError("The input must be numeric.")
+
     return x + 273
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-"""Tests all code in src.learners.utils."""
+"""Tests all code in src.weather.utils."""
 import unittest
 
 from src.weather.utils import celsius2kelvin
@@ -13,11 +13,10 @@ class TestUtils(unittest.TestCase):
         self.assertTrue(celsius2kelvin.__doc__)
     
     def test_celsius2kelvin_calculations(self):
-        """Correctness of the calculations"""
+        """Calculations are correct"""
         self.assertEqual(celsius2kelvin(-273), 0)
     
-    #def test_celsius2kelvin_input(self):
-    #    """Chekcs the input correctness"""
-    #    self.assertRaises(TypeError, celsius2kelvin, "string")
-
-    
+    def test_celsius2kelvin_input(self):
+        """The input is correct"""
+        self.assertRaises(TypeError, celsius2kelvin, "string")
+   
