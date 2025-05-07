3e18fec5e836d8114756dbb3b7d4c62c81964eee
VickoT <vicko.tornblom@gmail.com>
2025-05-07 14:46:20 +0200

    Test for get_digits, issue #45

--- tests/test_get_digits.py
+++ tests/test_get_digits.py
@@ -11,3 +11,8 @@ class TestGetDigits(unittest.TestCase):
     def test_get_digits(self):
         """The function 'get_name' returns the correct name."""
         self.assertEqual(get_digits(123),[1,2,3])
+    
+    def test_get_digits_raises_an_exception_upon_non_ints(self):
+        """The function 'is_zero' raises an exception upon non-ints."""
+        self.assertRaises(TypeError, get_digits, {1, 2})
+        self.assertRaises(TypeError, get_digits, "I am a string")
