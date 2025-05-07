298e87f8a188d5585eccc9c66542e58d485ef68b
Andrei Guliaev <andrei.guliaev@imbim.uu.se>
2025-05-07 13:58:23 +0200

    add stub of test, progress #40

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,13 @@
 """Tests all code in src.learners.utils."""
 import unittest
 
-from src.learners.utils import get_name
+from src.weather.utils import celsius2kelvin
 
 
 class TestUtils(unittest.TestCase):
 
-    """Class to test the code in src.learners.utils."""
+    """Class to test the code in src.weather.utils."""
 
-    def test_get_name(self):
-        """The function 'get_name' returns the correct name."""
-        self.assertEqual(get_name(), "utils")
+    def test_celsius2kelvin(self):
+        """The function 'celsius2kelvin' returns the correct temperature."""
+        self.assertEqual(celsius2kelvin(-273), 0)
