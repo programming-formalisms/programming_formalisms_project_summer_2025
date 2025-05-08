1f46ca2524fa2e0e2bd50e1bc3e8dae4553509b3
xiuqij <lostld97@gmail.com>
2025-05-08 13:32:20 +0200

    add class in reader.py

--- src/weather/reader.py
+++ src/weather/reader.py
@@ -14,4 +14,28 @@ def read_data(filepath:str):
     
     if len(infile_list) >0:
         return True
-    
+
+class Date:
+    def __init__(self, y:int,m:int,d:int):
+      self.year = y
+      self.month = m
+      self.day = d
+    def __str__(self):
+        return f'{self.year}-{self.month}-{self.day}'
+
+class Temperature:
+    def __init__(self, temp:float, temp_corrected:float):
+      self.temp = temp
+      self.temp_corrected = temp_corrected
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
+
+class DataPoint:
+    def __init__(self, any_position, any_velocity):
+      self.position = any_position
+      self.velocity = any_velocity
+    def __str__(self):
+        return (
+            "Position: " + str(self.position)  + ", "
+            + "velocity" + str(self.velocity)
+        )
