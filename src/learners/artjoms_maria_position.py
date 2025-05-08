3a3ed39ae69a5667a3b8fbaf2d1010200f6034e6
artheisten <art3mij@gmail.com>
2025-05-08 13:15:18 +0200

    first test run

--- src/learners/artjoms_maria_position.py
+++ src/learners/artjoms_maria_position.py
@@ -1,13 +1,11 @@
 class Position:
     def __init__(self, any_x, any_y):
+        if not (isinstance(any_x, [int, float]) and isinstance(any_y, [int, float])):
+            raise TypeError("At least One of the positional argument is non-numeric")
+
         self.x = any_x
         self.y = any_y
     def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
     def __repr__(self):
         return 'Position'
-
-a = Position(3.13,927) 
-print(a)
-print(type(a))
-assert str(type(a)) == "<class '__main__.Position'>"
--- tests/test_artjoms_maria_position_utils.py
+++ tests/test_artjoms_maria_position_utils.py
@@ -3,5 +3,9 @@ import unittest
 from src.learners.artjoms_maria_position import Position
 
 class TestPosition(unittest.TestCase):
+    
     def test_input_type(self):
-        self.assertRaises(TypeError, Position(), ("string", 0))
+        """A TypeError is raised for non-numeric input"""
+        self.assertRaises(TypeError, Position, "string0", "string1")
+        self.assertRaises(TypeError, Position, 0, "string1")
+        self.assertRaises(TypeError, Position, "string1", 1)
